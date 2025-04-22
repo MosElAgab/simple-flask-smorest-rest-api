19920a2c18754b73f0d7c21b6bd90c47cec75a99
MosElAgab <mostafa.elagab@gmail.com>
2025-04-22 11:49:14 +0100

    adds TagModel

--- models/store.py
+++ models/store.py
@@ -8,3 +8,4 @@ class StoreModel(db.Model):
     store_name = db.Column(db.String(80), unique=True, nullable=False)
 
     items = db.relationship("ItemModel", back_populates="store", lazy="dynamic", cascade="all, delete, delete-orphan")
+    tags = db.relationship("TagModel", back_populates="store", lazy="dynamic")
---
+++ models/tag.py
@@ -0,0 +1,12 @@
+from db import db
+
+
+class TagModel(db.Model):
+    __tablename__ = "tags"
+
+    tag_id = db.Column(db.Integer, primary_key=True)
+    tag_name = db.Column(db.String(80), unique=False, nullable=False)
+    store_id = db.Column(db.Integer, db.ForeignKey("stores.store_id"), unique=False, nullable=False)
+    
+
+    store = db.relationship("StoreModel", back_populates="tags")
