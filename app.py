26c4d2f765d1e15468b1edaab03cc54c43be3984
MosElAgab <mostafa.elagab@gmail.com>
2025-04-22 09:38:43 +0100

    adds sqlalchemy configs to app

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_smorest import Api
 
+from db import db
 from resources.store import blp as StoreBlueprint
 from resources.item import blp as ItemBlueprint
 
-def create_app():
+def create_app(db_url=None):
     app = Flask(__name__)
 
 
@@ -15,12 +16,15 @@ def create_app():
     app.config["OPENAPI_URL_PREFIX"] = "/"
     app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
     app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 
     @app.route("/")
     def home():
-        return "hello, World!"
-
+        return "Hello to Simple-Flask-Smorest-REST-API"
+    
+    db.init_app(app)
 
     api = Api(app)
 
