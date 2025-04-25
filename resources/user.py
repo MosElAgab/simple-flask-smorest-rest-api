8a299a991ab69d1265a1510130b392776b5fb093
MosElAgab <mostafa.elagab@gmail.com>
2025-04-25 12:27:49 +0100

    adds /register resource

---
+++ resources/user.py
@@ -0,0 +1,30 @@
+from flask.views import MethodView
+from flask_smorest import Blueprint, abort
+from passlib.hash import pbkdf2_sha256
+from flask_jwt_extended import create_access_token
+
+from db import db
+from schema import UserSchema
+from models import UserModel
+
+
+blp = Blueprint("Users", __name__, description="Operations on users")
+
+
+@blp.route("/register")
+class UserRegister(MethodView):
+    @blp.arguments(UserSchema)
+    def post(self, user_data):
+        if UserModel.query.filter(
+            UserModel.username == user_data["username"]
+        ).first():
+            abort(409, message="A user with the given username already exists.")
+        
+        user = UserModel(
+            username=user_data["username"],
+            password=pbkdf2_sha256.hash(user_data["password"])
+        )
+        db.session.add(user)
+        db.session.commit()
+
+        return {"message": "User created successfully."}, 201
