9cb94424809258d441b446961182bc1751faf66a
MosElAgab <mostafa.elagab@gmail.com>
2025-05-01 23:53:33 +0100

    adds fixtures for testing in conftest.py

--- test/unit_test.py/test_factory.py
+++ test/unit_test.py/test_factory.py
@@ -1,8 +1,14 @@
+
 import pytest
 from app import create_app
 
 
 def test_invalid_config_name_raises_value_error():
+    """
+    Given create_app function
+    When invlaid config_name is give
+    Then ensure it raises value error
+    """
     with pytest.raises(ValueError):
         create_app("invalid_config_name")
 
