05be369766dfeb0710e46e16a62333c4b283461b
MosElAgab <mostafa.elagab@gmail.com>
2025-06-23 22:01:09 +0100

    updates password in user schema to be required

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -4,4 +4,4 @@ from marshmallow import Schema, fields
 class UserSchema(Schema):
     user_id = fields.Int(dump_only=True)
     username = fields.Str(required=True)
-    password = fields.Str(load_only=True)
+    password = fields.Str(required=True, load_only=True)
