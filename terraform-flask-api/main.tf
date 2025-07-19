85798206b28642e3358f05803709ca7687fad4bd
MosElAgab <mostafa.elagab@gmail.com>
2025-07-19 23:48:01 +0100

    adds user-data to ec2 resource

--- terraform-flask-api/main.tf
+++ terraform-flask-api/main.tf
@@ -37,8 +37,21 @@ resource "aws_instance" "flask_server" {
   tags = {
     Name = "flask-api-server"
   }
-}
+user_data = <<-EOF
+  #!/bin/bash
 
+  sudo dnf update -y
+  sudo dnf install -y docker git make
+
+  sudo systemctl enable docker
+  sudo systemctl start docker
+
+  sudo usermod -aG docker ec2-user
+  
+  curl -SL https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+  chmod +x /usr/local/bin/docker-compose
+  EOF
+}
 output "instance_public_ip" {
   value = aws_instance.flask_server.public_ip
 }
